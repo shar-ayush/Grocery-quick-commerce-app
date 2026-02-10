1970c4aed6db18680de585087e0c9bab021ad181
Ayush Sharma <sharayush1441@gmail.com>
2026-02-10 06:33:21 +0530

    Add index file to export models for user, branch, category, product, order, and counter

---
+++ backend/src/models/index.js
@@ -0,0 +1,17 @@
+import {Customer, Admin, DeliveryPartner} from './user.js';
+import Branch from './branch.js';
+import Category from './category.js';
+import Product from './product.js';
+import Order from './order.js';
+import Counter from './counter.js';
+
+export {
+    Customer,
+    Admin,
+    DeliveryPartner,
+    Branch,
+    Category,
+    Product,
+    Order,
+    Counter
+}
