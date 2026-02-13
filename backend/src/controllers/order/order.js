ecfe2bab590423b4c69bd87d3a218ecd158ee724
Ayush Sharma <sharayush1441@gmail.com>
2026-02-13 12:46:01 +0530

    Add order controller with create, confirm, update status, and fetch orders functionalities

---
+++ backend/src/controllers/order/order.js
@@ -0,0 +1,143 @@
+import Order from '../../models/order.js';
+import Branch from '../../models/branch.js';
+import {Customer, DeiveryPartner } from '../../models/user.js'; 
+
+export const createOrder = async (req, reply) => {
+    try {
+        const {userId} = req.user;
+        const {items, branch, price} = req.body;
+
+        const customerData = await Customer.findOne(userId);
+        const branchData = await Branch.findOne(branch);
+
+        if(!customerData) {
+            return reply.status(404).send({ error: 'Customer not found.' });
+        }
+
+        const newOrder = new Order({
+            customer: userId,
+            items: items.map((item) => ({
+                id: item.id,
+                item: item.item,
+                count: item.count,
+            })),
+            branch,
+            totalPrice,
+            deliveryLocation:{
+                latitude: customerData.liveLocation.latitude,
+                longitude: customerData.liveLocation.longitude,
+                address: customerData.address || 'No address available',
+            },
+            pickupLocation:{
+                latitude: branchData.location.latitude,
+                longitude: branchData.location.longitude,
+                address: branchData.address || 'No address available', 
+            }, 
+        })
+
+        const savedOrder = await newOrder.save();
+        return reply.status(201).send(savedOrder);
+
+    } catch (error) {
+        console.error(error);
+        reply.status(500).send({ error: 'An error occurred while creating the order.' });
+    }
+}
+
+export const confirmOrder = async (req, reply) => {
+    try {
+        const {orderId} = req.params;
+        const {userId} = req.user;
+        const {deliveryPersonLocation} = req.body;
+
+        const deliveryPerson = await DeiveryPartner.findById(userId); 
+
+        if(!deliveryPerson) {
+            return reply.status(404).send({ message: 'Delivery partner not found.' });
+        }
+        
+        const oreder = await Order.findById(orderId);
+        if(!oreder) {
+            return reply.status(404).send({ message: 'Order not found.' });
+        }
+        if(oreder.status !== 'available') {
+            return reply.status(400).send({ message: 'Order is not available' });
+        }
+        order.status = 'confirmed';
+        order.deliveryPartner = userId;
+        order.deliveryPersonLocation = {
+            latitude: deliveryPersonLocation?.latitude,
+            longitude: deliveryPersonLocation?.longitude,
+            address: deliveryPersonLocation.address || "",
+        }
+
+        req.server.io.to(orderId).emit('orderConfirmed', order);
+        await order.save();
+
+        return reply.send(order);
+
+    } catch (error) {
+        return reply.status(500).send({ message: 'Failed to confirm the order', error });
+    }
+}
+
+export const updateOrderStatus = async (req, reply) => {
+    try {
+        const {orderId} = req.params;
+        const {status, deliveryPersonLocation} = req.body;
+        const {userId} = req.user; 
+
+        const deliveryPerson = await DeiveryPartner.findById(userId);
+        if(!deliveryPerson) {
+            return reply.status(404).send({ message: 'Delivery partner not found.' });
+        }
+
+        const order = await Order.findById(orderId);
+        if(!order) {
+            return reply.status(404).send({ message: 'Order not found.' });
+        }
+
+        if(["cancelled", "delivered"].includes(status)) {
+            return reply.status(400).send({ message: 'Order cannot be updated' });
+        }
+
+        if(order.deliveryPartner.toString() !== userId) {
+            return reply.status(403).send({ message: 'Unauthorized' });
+        }
+        order.status = status;
+        order.deliveryPersonLocation = deliveryPersonLocation;
+        await order.save();
+
+        req.server.io.to(orderId).emit('liveTrackingUpdates', order);
+        return reply.send(order);
+
+    } catch (error) {
+        return reply.status(500).send({ message: 'Failed to update order status', error });
+    }
+}
+
+export const getOrders = async (req, reply) => {
+    try {
+        const {status, customerId, deliveryPartnerId, branchId} = req.query;
+        let query = {};
+
+        if(status) {
+            query.status = status;
+        }
+        if(customerId) {
+            query.customer = customerId;
+        }
+        if(deliveryPartnerId) {
+            query.deliveryPartner = deliveryPartnerId;
+            query.branch = branchId;
+        }
+        
+        const orders = await Order.find(query).populate(
+            "customer branch items.item deliveryPartner"
+        );
+
+        return reply.send(orders);
+    } catch (error) {
+        return reply.status(500).send({ message: 'Failed to fetch orders', error });
+    }
+}
