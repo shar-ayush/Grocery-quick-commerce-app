2c9bfc40bc32f3f1c3104b0dde04313d22b4a9f3
Ayush Sharma <sharayush1441@gmail.com>
2026-02-18 00:57:56 +0530

    Add order routes

---
+++ backend/src/routes/order.js
@@ -0,0 +1,23 @@
+import {
+    confirmOrder,
+    createOrder,
+    getOrderById,
+    getOrders,
+    updateOrderStatus,
+} from "../controllers/order/order.js";
+import { verifyToken } from "../middleware/auth.js";
+
+export const orderRoutes = async (fastify, options) => {
+    fastify.addHook("preHandler", async(req, reply) => {
+        const isAuthenticated = await verifyToken(req, reply);
+        if(!isAuthenticated) {
+            return reply.status(401).send({ message: "Unauthorized" });
+        }
+    });
+
+    fastify.post("/order", createOrder);
+    fastify.get("/order", getOrders);
+    fastify.patch("/order/:orderId/status", updateOrderStatus);
+    fastify.post("/order/:orderId/confirm", confirmOrder);
+    fastify.get("/order/:orderId", getOrderById);
+}
